637f19e5dcec3bd27e1e2fe366c054798d64bc16
iwantbread <popeoompf@gmail.com>
2024-03-29 21:05:34 -0700

    v1.2.13, rotundpup energy again

--- SlugpupStuff.cs
+++ SlugpupStuff.cs
@@ -18,7 +18,7 @@ using HUD;
 
 namespace SlugpupStuff
 {
-    [BepInPlugin(MOD_ID, "Slugpup Stuff", "1.2.12")]
+    [BepInPlugin(MOD_ID, "Slugpup Stuff", "1.2.13")]
     public partial class SlugpupStuff : BaseUnityPlugin
     {
         public const string MOD_ID = "iwantbread.slugpupstuff";
